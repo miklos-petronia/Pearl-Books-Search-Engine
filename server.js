379bdbb6ae603bd309d516abbf2b46ce1b1a2020
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-14 21:48:47 +0100

    app usage.routes

--- server.js
+++ server.js
@@ -30,5 +30,29 @@ if (process.env.NODE_ENV === "production") {
 }
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));// app.use(routes);
+    
+    db.once("open", () => {
+      app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        // log where we can go to test our GQL API
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+      });
+    });
+    db.on("error", (err) => {
+      console.error("MongoDB connection error: ", err);
+    });
+});
+
+// app.use(routes);
+
+db.once("open", () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+        // log where we can go to test our GQL API
+        console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+});
+db.on("error", (err) => {
+    console.error("MongoDB connection error: ", err);
 });
