ae76e4e83e7c52f4132065358136914ba22631dd
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-14 21:45:52 +0100

    Develop a new Apollo server and pass in our schema data

--- server.js
+++ server.js
@@ -10,3 +10,10 @@ const { typeDefs, resolvers } = require("./schemas");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+});
