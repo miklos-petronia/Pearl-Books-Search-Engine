c106ff898d22949f2a8348942b10f109b6717d96
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-14 17:51:24 +0100

    developing a use effect to save books id

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -24,3 +24,42 @@ const SearchBooks = () => {
     const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
     const [saveBook] = useMutation(SAVE_BOOK);
+
+    // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
+    // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
+    useEffect(() => {
+        return () => saveBookIds(savedBookIds);
+    });
+
+    // create method to search for books and set state on form submit
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!searchInput) {
+            return false;
+        }
+
+        try {
+            const response = await searchGoogleBooks(searchInput);
+
+            if (!response.ok) {
+                throw new Error("something went wrong!");
+            }
+
+            const { items } = await response.json();
+
+            const bookData = items.map((book) => ({
+                bookId: book.id,
+                authors: book.volumeInfo.authors || ["No author to display"],
+                title: book.volumeInfo.title,
+                description: book.volumeInfo.description,
+                link: book.volumeInfo.infoLink,
+                image: book.volumeInfo.imageLinks?.thumbnail || "",
+            }));
+
+            setSearchedBooks(bookData);
+            setSearchInput("");
+        } catch (err) {
+            console.error(err);
+        }
+    };
