7b6f0fa77938e885124d25b12b3622a171566d8b
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-14 19:51:56 +0100

    create a serach to google books api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -49,3 +49,9 @@ export const deleteBook = (bookId, token) => {
         },
     });
 };
+
+// Create a search to google books api
+// https://www.googleapis.com/books/v1/volumes?q=harry+potter
+export const searchGoogleBooks = (query) => {
+    return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
+};
