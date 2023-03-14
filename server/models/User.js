d9e95759bec3e66247805d97bfad34e4d3427ac3
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-14 21:26:11 +0100

    when query a user, we will also obatin another field called  with the number of saved books we have

--- server/models/User.js
+++ server/models/User.js
@@ -41,3 +41,17 @@ userSchema.pre('save', async function (next) {
 
     next();
 });
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+// when query a user, we will also obatin another field called `bookCount` with the number of saved books we have
+userSchema.virtual('bookCount').get(function () {
+    return this.savedBooks.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
