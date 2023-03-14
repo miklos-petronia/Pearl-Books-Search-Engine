b53200fe7fbff3b982a928572a3a3384d671c443
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-14 21:21:25 +0100

    import schema from Book.js

---
+++ server/models/User.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// import schema from Book.js
+const bookSchema = require('./Book');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [/.+@.+\..+/, 'Must use a valid email address'],
+        },
+        password: {
+            type: String,
+            required: true,
+        },
