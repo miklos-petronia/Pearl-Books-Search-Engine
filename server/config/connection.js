7f7b4579be870ed635dc234719b2a201151c15aa
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-14 21:12:04 +0100

    connect to mongodb

---
+++ server/config/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+//connect to mongodb atlas
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+});
+
+module.exports = mongoose.connection;
