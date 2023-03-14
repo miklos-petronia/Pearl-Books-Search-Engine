60547a8626a622805dcd8a0757098cf300aa07f3
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-14 21:24:05 +0100

    arrange the savedBooks to be an array of data that adheres to the bookSchema and arrange the virtual below

--- server/models/User.js
+++ server/models/User.js
@@ -21,3 +21,13 @@ const userSchema = new Schema(
             type: String,
             required: true,
         },
+        // arrange the savedBooks to be an array of data that adheres to the bookSchema
+        savedBooks: [bookSchema],
+    },
+    // arrange this to use virtual below
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
