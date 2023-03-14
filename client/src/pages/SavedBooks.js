b6299057c44961fa10026a00b63a0575ba3fa77b
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-14 17:38:11 +0100

    Provide information if data isn't here yet

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -41,3 +41,54 @@ const SavedBooks = () => {
             console.error(err);
         }
     };
+
+    // if data isn't here yet, say so
+    if (loading) {
+        return <h2>LOADING...</h2>;
+    }
+
+    return (
+        <>
+            <div fluid="true" className='text-light bg-dark p-5'>
+                <Container>
+                    <h1>Viewing saved books!</h1>
+                </Container>
+            </div>
+            <Container>
+                <h2>
+                    {userData.savedBooks.length
+                        ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? "book" : "books"
+                        }:`
+                        : "You have no saved books!"}
+                </h2>
+                <Row>
+                    {userData.savedBooks.map((book) => {
+                        return (
+                            <Col key={book.bookId} md="4" >
+                                <Card key={book.bookId} border="dark">
+                                    {book.image ? (
+                                        <Card.Img
+                                            src={book.image}
+                                            alt={`The cover for ${book.title}`}
+                                            variant="top"
+                                        />
+                                    ) : null}
+                                    <Card.Body>
+                                        <Card.Title>{book.title}</Card.Title>
+                                        <p className='small'>Authors: {book.authors}</p>
+                                        <Card.Text>{book.description}</Card.Text>
+                                        <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                                            Delete this Book!
+                                        </Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        );
+                    })}
+                </Row>
+            </Container>
+        </>
+    );
+};
+
+export default SavedBooks;
