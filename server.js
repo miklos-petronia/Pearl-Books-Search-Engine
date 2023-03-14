f7f3cb9c7b16e812875f9c88668084e391d288b1
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-14 21:46:59 +0100

    combine our Apollo server with the Express application as middleware

--- server.js
+++ server.js
@@ -11,9 +11,15 @@ const { typeDefs, resolvers } = require("./schemas");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// create a new Apollo server and pass in our schema data
+// Develop a new Apollo server and pass in our schema data
 const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware,
 });
+
+// combine our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
