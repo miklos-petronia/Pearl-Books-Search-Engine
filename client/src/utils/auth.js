e34c74bbe8c67653e4846d022da85bd46d5c0809
miklos.petronia@hotmail.com <miklos.petronia@hotmail.com>
2023-03-14 19:55:51 +0100

    Developing a new class to launch for a user

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,2 +1,9 @@
 // utilize this to decode a token and obtain the user's information 
 import decode from 'jwt-decode';
+
+// Developing a new class to launch for a user
+class AuthService {
+    // obatin a user data information
+    getProfile() {
+        return decode(this.getToken());
+    }
